3feb3200d851c93cd58568615c7ae834c8db7aa5
tiluuta <tiluutan@gmail.com>
2023-11-30 20:57:06 -0800

    Push Maia's display.js

--- display.js
+++ display.js
@@ -1,5 +1,6 @@
 // p5.js script for visualizing sentiment data
 
+/*
 let sentimentScore;
 let sentimentMagnitude;
 
@@ -65,4 +66,77 @@ function draw() {
     // Draw the ellipse
     ellipse(e.locx, e.locy, e.size, e.size);
   }
+}
+ */
+
+let fireParticles = [];
+let sentimentScore = 0; //default score
+let sentimentMagnitude = 0; //default magnitude
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  frameRate(30);
+
+  //initialize  particles
+  for (let i = 0; i < 150; i++) {
+    fireParticles.push(createParticle());
+  }
+}
+
+function draw() {
+  //fetch the latest sentiment data
+  fetchSentimentData();
+
+  //clear canvas
+  background(0);
+
+  //update and draw each fire particle
+  for (let particle of fireParticles) {
+    updateParticle(particle);
+    drawParticle(particle);
+  }
+}
+
+function fetchSentimentData() {
+  fetch('http://localhost:8000/api/v1/sentiment')
+    .then(response => response.json())
+    .then(data => {
+      sentimentScore = data.sentiment_score;
+      sentimentMagnitude = data.sentiment_magnitude;
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+function createParticle() {
+  let direction = random(-1, 1);
+  let speed = random(0.01, 0.04);
+  return {
+    x: width / 2,
+    y: height / 2,
+    angle: random(0, TWO_PI),
+    radius: random(50, 200),
+    speed: direction * speed,
+    color: color(random(200, 255), random(50), 0, 150),
+  };
+}
+
+function updateParticle(particle) {
+  //adjust speed based on sentiment magnitude
+  particle.speed += sentimentMagnitude * 0.001;
+
+  //update angle
+  particle.angle += particle.speed;
+
+  //adjust color based on sentiment score
+  particle.color = lerpColor(color(255, 0, 0), color(0, 255, 0), (sentimentScore + 1) / 2);
+}
+
+function drawParticle(particle) {
+  let x = particle.x + cos(particle.angle) * particle.radius;
+  let y = particle.y + sin(particle.angle) * particle.radius;
+
+  noStroke();
+  fill(particle.color);
+  ellipse(x, y, 10, 10);
 }
